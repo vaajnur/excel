b394bfab224f98cc789595e9ae4ba927f325e34f
vaajnur <ainur.valiev@mail.ru>
2017-03-14 15:57:58 +0300

    change

--- install/codebase/php/config.php
+++ install/codebase/php/config.php
@@ -2,11 +2,11 @@
 $db_host = 'localhost';
 $db_port = '3306';
 $db_user = 'root';
-$db_pass = 'vfneh44';
+$db_pass = '';
 $db_name = 'bitrix_26_06';
 $db_prefix = '__';
 $db_type = 'MySQLi';
 $username = 'root';
-$password = 'vfneh44';
+$password = '';
 require_once('db_mysqli.php');
 ?>
